96d70a9996c9119450af6e7286338814d9e31867
Coldivy <53863879+Coldivy@users.noreply.github.com>
2025-10-03 11:57:53 +0800

    Add files via upload

---
+++ basicFrame.py
@@ -0,0 +1,69 @@
+from PySide6.QtWidgets import QApplication,QWidget,QLineEdit
+from calcu import Ui_Form
+from ctypes import *
+
+c_lib = CDLL('./cal_c.dll')
+
+c_lib.calculate.restype = c_double
+
+def python_calculate(expression):
+    # 将Python字符串编码为C字符串
+    c_expression = expression.encode('utf-8')
+    # 调用C函数
+    result = c_lib.calculate(c_expression)
+    return result
+
+class MyWindows(QWidget,Ui_Form):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.bind()
+
+        self.result = ''
+
+    def bind(self):
+        self.pushButton_0.clicked.connect(lambda : self.addNumber('0'))
+        self.pushButton_1.clicked.connect(lambda : self.addNumber('1'))
+        self.pushButton_2.clicked.connect(lambda : self.addNumber('2'))
+        self.pushButton_3.clicked.connect(lambda : self.addNumber('3'))
+        self.pushButton_4.clicked.connect(lambda : self.addNumber('4'))
+        self.pushButton_5.clicked.connect(lambda : self.addNumber('5'))
+        self.pushButton_6.clicked.connect(lambda : self.addNumber('6'))
+        self.pushButton_7.clicked.connect(lambda : self.addNumber('7'))
+        self.pushButton_8.clicked.connect(lambda : self.addNumber('8'))
+        self.pushButton_9.clicked.connect(lambda : self.addNumber('9'))
+        self.pushButton_plus.clicked.connect(lambda : self.addNumber('+'))
+        self.pushButton_dot.clicked.connect(lambda : self.addNumber('.'))
+        self.pushButton_sub.clicked.connect(lambda : self.addNumber('-'))
+        self.pushButton_mul.clicked.connect(lambda : self.addNumber('*'))
+        self.pushButton_div.clicked.connect(lambda : self.addNumber('/'))
+        self.pushButton_equal.clicked.connect(self.equal)
+        self.pushButton_clear.clicked.connect(self.clear)
+        self.pushButton_back.clicked.connect(self.back)
+
+    def addNumber(self,number):
+        self.lineEdit.clear()
+        self.result += number
+        self.lineEdit.setText(str(self.result))
+
+    def equal(self):
+        self.result = str(python_calculate(self.result))
+        self.lineEdit.setText(str(self.result))
+
+    def clear(self):
+        self.result = ''
+        self.lineEdit.setText(str(self.result))
+
+    def back(self):
+        self.result = self.result[:-1]
+        if self.result[-1] == '.':
+            self.result = self.result[:-1]
+
+        self.lineEdit.setText(str(self.result))
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MyWindows()
+    window.show()
+    app.exec()
